ed764237b8bf0a55cf1107b30877e8d464581972
faraahahmeed <faraahahmeed@outlook.com>
2023-06-25 21:24:54 +0300

    fixed import

--- lib/Screens/Client Screens/RateScreen.dart
+++ lib/Screens/Client Screens/RateScreen.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
-import 'package:myapp/Screens/scheduleCard.dart';
+import 'package:myapp/Screens/Client%20Screens/scheduleCard.dart';
 
 class RateScreen extends StatefulWidget{
   final String id;
